3d92ec8cdd1a1759592c99310242bde77d37c812
unisanta <unisanta@M216-09>
2023-10-27 21:38:48 -0300

    Add project files.

---
+++ Concesionaria/Controllers/ConcesionariaController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Concesionaria.Models;
+
+namespace Concesionaria.Controllers
+{
+    public class ConcesionariaController : Controller
+    {
+        private readonly List<Conc> carros = new List<Conc>();
+
+        public ConcesionariaController()
+        {
+            carros.Add(new Conc()
+            {
+                id = "0001",
+                marca = "Mercedes",
+                modelo = "GLE 400",
+                cor = "Preto",
+                valor = 750900
+            });
+
+            carros.Add(new Conc()
+            {
+                id = "0002",
+                marca = "Range Ronver",
+                modelo = "Velar",
+                cor = "Vermelho",
+                valor = 624900
+            });
+        }
+        
+
+        public IActionResult Index()
+        {
+            return View(carros);
+        }
+
+        public IActionResult Details(string id)
+        {
+            var carro = carros.Find(c => c.id == id);
+            if (carro == null) return NotFound();
+
+            return View(carro);
+
+        }
+
+
+
+    }
+}
---
+++ Concesionaria/Models/Conc.cs
@@ -0,0 +1,13 @@
+﻿namespace Concesionaria.Models
+{
+    public class Conc
+    {
+        public string id { get; set; } = String.Empty;
+        public string marca { get; set; } = String.Empty;
+        public string modelo { get; set; } = String.Empty;
+        public string cor { get; set; } = String.Empty;
+        public double valor { get; set; }
+
+
+    }
+}
